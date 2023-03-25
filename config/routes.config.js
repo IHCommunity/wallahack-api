6cd04b974ed1f63c79773d974d2ccc3848f91df0
Pablo Laso Cortabitarte <plasocortabitarte@gmail.com>
2023-03-25 11:53:40 +0100

    feat: get user by id endpoint

--- config/routes.config.js
+++ config/routes.config.js
@@ -5,5 +5,6 @@ const usersController = require('../controllers/users.controller');
 
 router.post('/users', usersController.create);
 router.get('/users', usersController.list);
+router.get('/users/:id', usersController.getUser);
 
 module.exports = router;
--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -1,4 +1,5 @@
 const User = require('../models/User.model');
+const createError = require('http-errors');
 const { StatusCodes } = require('http-status-codes');
 
 // { "email": "pablo@email.com", "password": "12345678" }
@@ -14,5 +15,17 @@ module.exports.create = (req, res, next) => {
 module.exports.list = (req, res, next) => {
   User.find()
     .then(users => res.json(users))
+    .catch(next)
+}
+
+module.exports.getUser = (req, res, next) => {
+  User.findById(req.params.id)
+    .then(user => {
+      if (!user) {
+        next(createError(StatusCodes.NOT_FOUND, 'User not found'))
+      } else {
+        res.json(user);
+      }
+    })
     .catch(next)
 }
