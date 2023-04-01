b717f5ea829975246a4add8c647787c5b1ae7e52
Cdelatorre <carlosdelatorrediaz@gmail.com>
2023-04-01 12:52:27 +0200

    crud completed

--- config/routes.config.js
+++ config/routes.config.js
@@ -1,6 +1,8 @@
 const router = require('express').Router();
 const authController = require('../controllers/auth.controller');
 const usersController = require('../controllers/users.controller');
+const productsController = require('../controllers/products.controller');
+const upload = require('../config/storage.config');
 
 const authMiddleware = require('../middlewares/auth.middleware');
 
@@ -14,5 +16,12 @@ router.post('/users', usersController.create);
 router.get('/users', usersController.list);
 router.get('/users/me', authMiddleware.isAuthenticated, usersController.getCurrentUser);
 router.get('/users/:id', usersController.getUser);
+
+/* Products */
+
+router.post('/products', authMiddleware.isAuthenticated, upload.single('photo'), productsController.create);
+router.get('/products', productsController.list);
+router.patch('/products/:id', authMiddleware.isAuthenticated, productsController.buy)
+router.get('/products/:id', productsController.detail)
 
 module.exports = router;
